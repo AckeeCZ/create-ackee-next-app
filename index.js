2af798664ac9785366a02f13dd5e8a1a01d1f453
Lukas Horak <horaklukas@centrum.cz>
2022-07-21 20:21:45 +0200

    ✨ Use template path

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
 const { spawn } = require("child_process");
 
 const TEMPLATE_URL = `https://github.com/AckeeCZ/next-app-template`;
+const TEMPLATE_PATH = "template";
 
-spawn("yarn", ["create", "next-app", `--example=${TEMPLATE_URL}`], {
-  stdio: "inherit",
-});
+spawn(
+  "yarn",
+  [
+    "create",
+    "next-app",
+    `--example=${TEMPLATE_URL}`,
+    `--example-path=${TEMPLATE_PATH}`,
+  ],
+  {
+    stdio: "inherit",
+  }
+);
