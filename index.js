356abac67aaa759c0a5eafccc0e3d4f0c991b7b9
Lukas Horak <horaklukas@centrum.cz>
2022-07-21 13:01:49 +0200

    🎉 First version of the package

---
+++ index.js
@@ -0,0 +1,7 @@
+const { spawn } = require("child_process");
+
+const TEMPLATE_URL = `https://github.com/AckeeCZ/next-app-template`;
+
+spawn("yarn", ["create", "next-app", `--example=${TEMPLATE_URL}`], {
+  stdio: "inherit",
+});
